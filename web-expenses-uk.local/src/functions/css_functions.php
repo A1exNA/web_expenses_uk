b3c1a63a6250b9a7a626fdbf3f6a8b754d9b198e
Alexander N. <alexnazim.811@gmail.com>
2025-11-18 14:04:26 +0300

    css helper merge

---
+++ web-expenses-uk.local/src/functions/css_functions.php
@@ -0,0 +1,30 @@
+<?php
+
+/* ----- CSS соединение файла ----- */
+function mergeCssFiles() {
+    $files = [
+        '../../src/css/general.css',
+        '../../src/css/fonts.css',
+        '../../src/css/header.css',
+        '../../src/css/main.css',
+        '../../src/css/footer.css'
+    ];
+
+    $style_css = '';
+    foreach ($files as $file) {
+        $style_css .= file_get_contents($file);
+    }
+
+    $outputFile = '../css/style.css';
+    $needWrite = true;
+    if (file_exists($outputFile)) {
+        $existingContent = file_get_contents($outputFile);
+        if ($existingContent === $style_css) {
+            $needWrite = false;
+        }
+    }
+
+    return [$needWrite, $outputFile, $style_css];
+}
+
+?>
--- web-expenses-uk.local/src/templates/header.php
+++ web-expenses-uk.local/src/templates/header.php
@@ -4,10 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <title><?= $title ?></title>
+    <?php
+    require_once '../../src/functions/css_functions.php';
+    $variable_array = mergeCssFiles();
+    $needWrite = $variable_array[0];
+    $outputFile = $variable_array[1];
+    $style_css = $variable_array[2];
+    ?>
     <link rel="stylesheet" href="../css/style.css">
 </head>
 
-<body>
+<body class="font__main">
     <header>
-        <div class="font__main h1">УК Тверьжилфонд</div>
+        <div class="h1">УК Тверьжилфонд</div>
     </header>
+    <main>
+        <?php
+        if ($needWrite) {
+            file_put_contents($outputFile, $style_css);
+            echo 'Файл обновлён';
+        } else {
+            echo 'Изменений нет, файл не перезаписан';
+        }
+        ?>
+    </main>
