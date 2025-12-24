d822b1ea7cefa13982b9d6b87b241a47f1cb919e
Alexander N. <alexnazim.811@gmail.com>
2025-12-24 19:34:00 +0300

    requests via JSON

---
+++ web-expenses-uk.local/src/database/fetcher.php
@@ -0,0 +1,119 @@
+<?php 
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+$action = $data['action'];
+$headers = $data['headers'];
+$database = $data['database'];
+
+$innerHTML = "";
+
+if ($action === 'Create') {
+    foreach ($headers as $header) {
+        $header_name = $header['name'];
+        if ($header_name == 'id') {
+            $value = count($database) + 1;
+        } else if ($header_name == 'object_address') {
+            $value = "г. Тверь, ул/пер.";
+        } else {
+            $value = '';
+        };
+
+        $innerHTML .= <<< HTML
+        <div class='input__block'>
+            <div class='block__element'>$header_name</div>
+            <div class='block__element'><input class='font__input' type='text' name='$header_name' value='$value' placeholder='Введите $header_name' autocomplete='off'></div>
+        </div>
+        HTML;
+    };
+    $innerHTML .= <<< HTML
+        <div class='input__block'>
+            <button class='font__input' type='submit'>Отправить данные</button>
+        </div>
+        HTML;
+
+    echo json_encode([
+        "status" => "success", 
+        "message" => "Все хорошо", 
+        "data" => $data,
+        "action" => $action,
+        "headers" => $headers,
+        "database" => $database,
+        "innerHTML" => $innerHTML
+    ]);
+} else if ($action === 'Change') {
+    $innerHTML .= <<< HTML
+        <div class="input__block">
+            <div class="block__element">id</div>
+            <div class="block__element">
+                <input class="font__input" id="input" list="id_deleted" type="text" name="id" placeholder="Введите id" autocomplete="off">
+                <datalist id="id_deleted">
+    HTML;
+
+    foreach ($database as $dataB) {
+        $id_data = $dataB['id'];
+        $name_data = $dataB['object_address'];
+        $innerHTML .= <<< HTML
+                <option value='$id_data'>$name_data
+        HTML;
+    }
+
+    $innerHTML .= <<< HTML
+            </datalist>
+        </div>
+    </div>
+    HTML;
+
+    foreach ($headers as $header) {
+        $header_name = $header['name'];
+
+        if ($header_name == 'id') {
+            continue;
+        };
+
+        $innerHTML .= <<< HTML
+        <div class='input__block'>
+            <div class='block__element'>$header_name</div>
+            <div class='block__element'><input class='font__input' type='text' name='$header_name' value='' placeholder='Введите $header_name' autocomplete='off'></div>
+        </div>
+        HTML;
+    };
+    $innerHTML .= <<< HTML
+        <div class="input__block">
+            <button class="font__input" type="submit">Изменить данные</button>
+        </div>
+    HTML;
+
+    echo json_encode([
+        "status" => "success",
+        "message" => "Все хорошо",
+        "data" => $data,
+        "action" => $action,
+        "headers" => $headers,
+        "database" => $database,
+        "innerHTML" => $innerHTML
+    ]);
+} else if ($action === 'Delete') {
+    echo json_encode([
+        "status" => "success",
+        "message" => "Все хорошо",
+        "data" => $data,
+        "action" => $action,
+        "headers" => $headers,
+        "database" => $database
+    ]);
+} else {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Все Плохо",
+        "data" => $data,
+        "action" => $action,
+        "headers" => $headers,
+        "database" => $database
+    ]);
+}
+
+exit;
+
+?>
--- web-expenses-uk.local/src/js/main.js
+++ web-expenses-uk.local/src/js/main.js
@@ -42,8 +42,6 @@ async function loadData(dbName) {
     const dbData = await dataSearch(dbName);
     const actionUrl = '../../src/database/server.php';
 
-    console.log(dbData);
-
     try {
         const response = await fetch(actionUrl, {
             method: 'POST',
@@ -68,45 +66,33 @@ async function loadData(dbName) {
 // Общая функция для взаимодействия с данными
 
 async function clickNavBtn(value, dbName) {
-    const data =  await dataSearch(dbName);
-    console.log(data);
-    headers = data['headers'];
-    database = data['database'];
-    console.log(headers);
-    console.log(database);
+    const dataS =  await dataSearch(dbName);
+    headers = dataS['headers'];
+    database = dataS['database'];
 
     const windowInput = document.getElementById('window__input');
     const headerNodeList = document.querySelectorAll('th');
 
-    let inputMenu = ``;
+    const actionUrl = '../../src/database/fetcher.php';
+    const response = await fetch (actionUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ action: value, headers: headers, database: database })
+    })
+
+    if (!response.ok) {
+        throw new Error(`Ошибка сервера: ${response.status}`)
+    };
+
+    const data = await response.json();
+
+    console.log(data);
+
+    windowInput.innerHTML = data['innerHTML'];
 
     if (value == "Create") {
-        let lengthArray = headers.length;
-
-        for (let i = 0; i < lengthArray; i++) {
-            valName = headerNodeList[i].textContent;
-
-            if (headers[i].name == "id") {
-                valValue = database.length + 1;
-            } else if (headers[i].name == "object_address") {
-                valValue = "г. Тверь, ул/пер.";
-            } else {
-                valValue = ""
-            }
-
-            inputMenu += `
-        <div class="input__block">
-            <div class="block__element">${valName}</div>
-            <div class="block__element"><input class="font__input" type="text" name="${headers[i].name}" value="${valValue}" placeholder="Введите ${valName}" autocomplete="off"></div>
-        </div>`;
-        }
-        inputMenu += `
-        <div class="input__block">
-            <button class="font__input" type="submit">Отправить данные</button>
-        </div>`;
-
-        windowInput.innerHTML = inputMenu;
-
         const form = document.getElementById('window__input');
 
         form.onsubmit = function(event) {
@@ -115,41 +101,6 @@ async function clickNavBtn(value, dbName) {
             submitFormData(form);
         };
     } else if (value == "Change") {
-        let lengthArray = database.length;
-        let lengthHeaders = headers.length;
-
-        inputMenu += `
-        <div class="input__block">
-            <div class="block__element">id</div>
-            <div class="block__element">
-                <input class="font__input" id="input" list="id_deleted" type="text" name="id" placeholder="Введите id" autocomplete="off">
-                <datalist id="id_deleted">`
-
-        for (let i = 0; i < lengthArray; i++) {
-            inputMenu += `
-                <option value="${database[i][0]}">${database[i][1]}`
-        }
-
-        inputMenu += `
-                </datalist>
-            </div>
-        </div>`
-
-        for (let i = 1; i < lengthHeaders; i++) {
-            valName = headerNodeList[i].textContent;
-            inputMenu += `
-        <div class="input__block">
-            <div class="block__element">${valName}</div>
-            <div class="block__element"><input class="font__input" id="${headers[i].name}" type="text" name="${headers[i].name}" value="" placeholder="Введите ${valName}" autocomplete="off"></div>
-        </div>`;
-        }
-
-        inputMenu += `
-        <div class="input__block">
-            <button class="font__input" type="submit">Изменить данные</button>
-        </div>`;
-
-        windowInput.innerHTML = inputMenu;
         const inputElement = document.getElementById('input')
 
         inputElement.addEventListener('change', function(event) {
@@ -158,17 +109,19 @@ async function clickNavBtn(value, dbName) {
             console.log('Введено число', selectedValue);
             console.log(headers, database);
 
-            for (let i = 1; i < lengthHeaders; i++) {
-                element = document.getElementById(headers[i].name);
+            for (let i = 1; i < headers.length; i++) {
+                element = document.getElementById(headers[i]['name']);
 
-                for (let j = 0; j < lengthArray; j++) {
+                for (let j = 0; j < database.length; j++) {
                     if (!selectedValue) {
                         console.log('Выберете id');
                         element.value = '';
                         break;
 
-                    } else if (database[j][0] == selectedValue) {
-                        element.value = database[j][i];
+                    } else if (database[j]['id'] == selectedValue) {
+                        console.log(database[j][headers[i]['name']]);
+
+                        element.value = database[j][headers[i]['name']];
                         break;
                     }
                 }
@@ -185,6 +138,8 @@ async function clickNavBtn(value, dbName) {
     } else if (value == "Delete") {
         let lengthArray = database.length;
 
+
+
         inputMenu += `
         <div class="input__block">
             <div class="block__element">id</div>
@@ -193,8 +148,9 @@ async function clickNavBtn(value, dbName) {
                 <datalist id="id_deleted">`
 
         for (let i = 0; i < lengthArray; i++) {
+            console.log(database);
             inputMenu += `
-            <option value="${database[i][0]}">${database[i][1]}`
+                <option value="${database[i][0]}">${database[i][1]}`
         }
 
 
